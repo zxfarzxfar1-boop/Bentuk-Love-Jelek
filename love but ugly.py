c8a807c50111c8915b27713338e57d1ddc8f1f5f
zxfarzxfar1-boop <zxfarzxfar1@gmail.com>
2025-11-11 16:21:20 +0700

    Add files via upload

---
+++ love but ugly.py
@@ -0,0 +1,13 @@
+
+def print_heart():
+    
+    for i in range(3):
+        print(" " * (3 - i) + "*" * (2 * i + 1) + " " * (3 - i) + "*" * (2 * i + 1))
+    
+    
+    for i in range(5, -1, -1):
+        print(" " * (5 - i) + "*" * (2 * i + 1))
+
+
+print_heart()
+''
